589ff32c37aebe7f1e2fae269e7a00ebed95ca1f
marek <marek.ondrejcik@ness.com>
2014-12-04 15:49:43 +0100

    commit

--- kuskodu.js
+++ kuskodu.js
@@ -24,67 +24,62 @@ function load(url, callback) {
 
 (function(window,document,undefined) {
   // 'use strict';
-  var Paginator = function() { 
-    var that = this; 
-    this.nextPage = this.nextPage.bind(this);
-    this.previousPage = this.previousPage.bind(this);
-    this.concretePage = this.concretePage.bind(this);
-    this.filter = this.filter.bind(this);
-    this.loadMore = this.loadMore.bind(this);
-    this.resize = this.resize.bind(this);
+  var Paginator = function(options) { 
+    this.resize_toggle = true;
+    this.data = [];
+    this.filteredData = [];
+    this.actualPage = 1;
+    this.actualItem = 0;
+    this.actualLastItem = 12;
+    this.numberOfPages = 0;
 
     this.settings = {
-      'itemsPerPage': 12,           
-      'actualPage': 1,
-      'actualItem': 0,
-      'actualLastItem': 12,
-      'numberOfPages': 0,
-      'container': "container",
-      'url': "http://academy.tutoky.com/api/json.php"
+      itemsPerPage: options.itemsPerPage || 12,
+      container: options.container || "container",
+      url: options.url || "http://academy.tutoky.com/api/json.php"
     }
-
-    // load JSON
-    load(this.settings.url, this.init.bind(this));
   }
 
   // save data to Paginator.data, init pagination and render articles
   Paginator.prototype.init = function(data){
-    Paginator.data = data;
-    this.originalData = data;   // used when data array is overwritten by filter function
-    this.renderArticles(paginator.data);
-    this.paginate();
+    this.data = data;
+    this.filteredData = data;   // used when data array is overwritten by filter function
+    this.renderArticles();
     this.showFilter();
   };
 
   // hide/show previous/next button, render pagination
   Paginator.prototype.paginate = function() {
-    document.getElementById("pages").innerHTML = "";
-    this.settings.numberOfPages = Math.ceil(Paginator.data.length / this.settings.itemsPerPage);
-    if (this.settings.actualPage === 1) {
-      document.getElementById("previous").style.visibility = "hidden";
-    }
-    else {
-      document.getElementById("previous").style.visibility = "initial";
-    }
-    if (this.settings.actualPage === this.settings.numberOfPages) {
-      document.getElementById("next").style.visibility = "hidden";
-    }
-    else {
-      document.getElementById("next").style.visibility = "initial";
-    }
+    document.getElementById("pagin").innerHTML = "";
     var show_dots = false;    // variable for checking if dots between pages were already shown
+    this.settings.numberOfPages = Math.ceil(this.filteredData.length / this.settings.itemsPerPage);
+    var fragment = document.createDocumentFragment(); 
+
+    var a_previous = document.createElement("a");
+    a_previous.id = "previous";
+    a_previous.href = "#";
+    a_previous.onclick = "previousPage()";
+    a_previous.textContent = "< PREVIOUS";
+    fragment.appendChild(a_previous);
+
+    var div = document.createElement("div");
+    div.id = "pages";
+
+    var fragment_pages = document.createDocumentFragment(); 
     // for loop for page numbers
     for (var i = 1; i < this.settings.numberOfPages + 1; i++) {
       // display page number if page is first, last, or if page is near actual page
-      if (i === 1 || i === this.settings.numberOfPages || (i < this.settings.actualPage + 3 && i > this.settings.actualPage - 2)) {
+      if (i === 1 || i === this.settings.numberOfPages || (i < this.actualPage + 3 && i > this.actualPage - 2)) {
         var a = document.createElement("a");   
         a.href = "#";
         a.textContent = i;
+        a.id = "page";
         a.setAttribute("data-page", i);
-        if (this.settings.actualPage === i) {
+        if (this.actualPage === i) {
           a.className = "active-page";
         }
-        document.getElementById('pages').appendChild(a);
+        div.appendChild(a);
+        fragment_pages.appendChild(a);
         show_dots = true;
       }
       // if page is not displayed, show dots between page numbers
@@ -92,10 +87,33 @@ function load(url, callback) {
         if (show_dots) {
           var dots = document.createElement("span");
           dots.textContent = "...";
-          document.getElementById('pages').appendChild(dots);
+          fragment_pages.appendChild(dots);
           show_dots = false;
         }
       }
+    }
+    div.appendChild(fragment_pages);
+    fragment.appendChild(div);
+
+    var a_next = document.createElement("a");
+    a_next.id = "next";
+    a_next.href = "#";
+    a_next.textContent = "NEXT >";
+    fragment.appendChild(a_next);
+
+    document.getElementById('pagin').appendChild(fragment);
+
+    if (this.actualPage === 1) {
+      document.getElementById("previous").style.visibility = "hidden";
+    }
+    else {
+      document.getElementById("previous").style.visibility = "initial";
+    }
+    if (this.actualPage === this.settings.numberOfPages) {
+      document.getElementById("next").style.visibility = "hidden";
+    }
+    else {
+      document.getElementById("next").style.visibility = "initial";
     }
   };
 
@@ -103,7 +121,7 @@ function load(url, callback) {
   Paginator.prototype.renderArticles = function() {
     var i;   
     var fragment = document.createDocumentFragment(); 
-    for(i = this.settings.actualItem; i < this.settings.actualLastItem; i++) {
+    for(i = this.actualItem; i < this.actualLastItem; i++) {
       var article = document.createElement("article");      
       article.id = i;
       var div = document.createElement("div");              
@@ -112,10 +130,10 @@ function load(url, callback) {
       var div_hover = document.createElement("div");    
       div_hover.id = "hover";
       var p_hover = document.createElement("p");
-      p_hover.textContent = Paginator.data[i].title;  
+      p_hover.textContent = this.filteredData[i].title;  
       var p_categories = document.createElement("p");
       
-      var categories = (Paginator.data[i].categories).toString();
+      var categories = (this.filteredData[i].categories).toString();
       categories = categories.replace(/,/g , ", ")
       p_categories.textContent = categories; 
 
@@ -128,11 +146,11 @@ function load(url, callback) {
       img_placeholder.src = "placeholder.jpg";
 
       var h2 = document.createElement("h2");                         
-      h2.textContent = Paginator.data[i].title; 
+      h2.textContent = this.filteredData[i].title; 
 
       var time = document.createElement("time");   
-      time.datetime = Paginator.data[i].timestamp;
-      time.textContent = paginator.showTime(Paginator.data[i].timestamp);  
+      time.datetime = this.filteredData[i].timestamp;
+      time.textContent = myPagin.showTime(this.filteredData[i].timestamp);  
 
       article.appendChild(div); 
       article.appendChild(div_hover);
@@ -148,7 +166,7 @@ function load(url, callback) {
     document.getElementById(this.settings.container).appendChild(fragment);   // load all articles from document fragment
     this.paginate();    
   };
-  // this.renderArticles = renderArticles;
+
   // parse date from json timestamp to readable output
   Paginator.prototype.showTime = function(date) {
     date = parseInt(date);
@@ -159,9 +177,9 @@ function load(url, callback) {
   // render categories names in select
   Paginator.prototype.showFilter = function() {
     var output = [];
-    for (var i = 0; i < Paginator.data.length; i++) {
-      for (var j = 0; j < Paginator.data[i].categories.length; j++) {
-        output.push(Paginator.data[i].categories[j]);
+    for (var i = 0; i < this.filteredData.length; i++) {
+      for (var j = 0; j < this.filteredData[i].categories.length; j++) {
+        output.push(this.filteredData[i].categories[j]);
       }
     }
     var unique = output.filter(function(item, i, ar){ return ar.indexOf(item) === i; });
@@ -181,64 +199,65 @@ function load(url, callback) {
 
   Paginator.prototype.nextPage = function() {
     document.getElementById(this.settings.container).innerHTML = "";
-    this.settings.actualPage++;
-    if ((this.settings.actualLastItem + this.settings.itemsPerPage) > Paginator.data.length) {
-      var lastPage = Paginator.data.length - this.settings.actualLastItem;
-      this.settings.actualLastItem += lastPage;
+    this.actualPage++;
+    if ((this.actualLastItem + this.settings.itemsPerPage) > this.filteredData.length) {
+      var lastPage = this.filteredData.length - this.actualLastItem;
+      this.actualLastItem += lastPage;
     }
     else {
-      this.settings.actualLastItem += this.settings.itemsPerPage;
+      this.actualLastItem += this.settings.itemsPerPage;
     }
-    this.settings.actualItem += this.settings.itemsPerPage;
+    this.actualItem += this.settings.itemsPerPage;
+    console.log(this.actualItem);
+    console.log(this.actualLastItem);
     document.getElementById(this.settings.container).innerHTML = "";
-    this.renderArticles(this.data);
+    this.renderArticles(this.filteredData);
   };
 
   // "load more" button in mobile version
   Paginator.prototype.loadMore = function() {
-    this.settings.actualPage++;
-    if ((this.settings.actualLastItem + this.settings.itemsPerPage) > Paginator.data.length) {
-      var lastPage = Paginator.data.length - this.settings.actualLastItem;
-      this.settings.actualLastItem += lastPage;
+    this.actualPage++;
+    if ((this.actualLastItem + this.settings.itemsPerPage) > this.filteredData.length) {
+      var lastPage = this.filteredData.length - this.actualLastItem;
+      this.actualLastItem += lastPage;
     }
     else {
-      this.settings.actualLastItem += this.settings.itemsPerPage;
+      this.actualLastItem += this.settings.itemsPerPage;
     }
-    this.settings.actualItem += this.settings.itemsPerPage;
-    this.renderArticles(Paginator.data);
+    this.actualItem += this.settings.itemsPerPage;
+    this.renderArticles(this.filteredData);
   };
 
   Paginator.prototype.previousPage = function() {
     document.getElementById(this.settings.container).innerHTML = "";
-    this.settings.actualPage--;
-    // document.getElementById("container").innerHTML = "";
-    if (this.settings.actualLastItem === Paginator.data.length) {
-      var lastPage = this.settings.actualLastItem - this.settings.actualItem;
-      this.settings.actualLastItem -= lastPage;
+    this.actualPage--;
+    if (this.actualLastItem === this.filteredData.length) {
+      var lastPage = this.actualLastItem - this.actualItem;
+      this.actualLastItem -= lastPage;
     }
     else {
-      this.settings.actualLastItem -= this.settings.itemsPerPage; 
+      this.actualLastItem -= this.settings.itemsPerPage; 
     }
-    this.settings.actualItem -= this.settings.itemsPerPage;
-    this.renderArticles(Paginator.data);
+    this.actualItem -= this.settings.itemsPerPage;
+    this.renderArticles(this.filteredData);
   };
 
-  Paginator.prototype.concretePage = function(event) {
+  Paginator.prototype.concretePage = function(page_number) {
     document.getElementById(this.settings.container).innerHTML = "";
-    var a_target = event.target;
-    var attribute = a_target.getAttribute("data-page");
-    attribute = parseInt(attribute);
-    if (attribute > 0) {    //check if event.target is a number
-      this.settings.actualPage = attribute;
-      this.settings.actualItem = (attribute - 1)  * this.settings.itemsPerPage;
+    // var a_target = event.target;
+    // var attribute = a_target.getAttribute("data-page");
+    // attribute = parseInt(attribute);
+    if (page_number > 0) {    //check if event.target is a number
+      this.actualPage = page_number;
+      this.actualItem = (page_number - 1)  * this.settings.itemsPerPage;
 
-      if (this.settings.numberOfPages === this.settings.actualPage) {
-        this.settings.actualLastItem = Paginator.data.length;
+      if (this.settings.numberOfPages === this.actualPage) {
+        this.actualLastItem = this.filteredData.length;
       }
       else {
-        this.settings.actualLastItem = this.settings.actualItem + this.settings.itemsPerPage;
+        this.actualLastItem = this.actualItem + this.settings.itemsPerPage;
       }
-      this.renderArticles(Paginator.data);
+      this.renderArticles(this.filteredData);
     }
   };
 
@@ -246,59 +265,77 @@ function load(url, callback) {
   Paginator.prototype.filter = function(event) {
     var cat_target = event.target;
     var attribute = cat_target.getAttribute("id");
+
     if (attribute === "all") {
-      Paginator.data = this.originalData;
+      this.filteredData = this.data;
       document.getElementById(this.settings.container).innerHTML = "";
-      this.settings.actualPage = 1;
-      this.settings.actualItem = 0;
-      this.settings.actualLastItem = this.settings.itemsPerPage;
-      this.renderArticles(Paginator.data);        
+      this.actualPage = 1;
+      this.actualItem = 0;
+      this.actualLastItem = this.settings.itemsPerPage;
+      this.renderArticles(this.filteredData);        
     }
     else if (attribute) {
       document.getElementById(this.settings.container).innerHTML = "";
       var output = [];
-      for (var i = 0; i < this.originalData.length; i++) {
-        for (var j = 0; j < this.originalData[i].categories.length; j++) {
-          if (this.originalData[i].categories[j] === attribute) {
-            output.push(this.originalData[i]);
+      for (var i = 0; i < this.data.length; i++) {
+        for (var j = 0; j < this.data[i].categories.length; j++) {
+          if (this.data[i].categories[j] === attribute) {
+            output.push(this.data[i]);
           }
         }
       }
-      Paginator.data = output;
-      this.settings.actualPage = 1;
-      this.settings.actualItem = 0;
-      this.settings.actualLastItem = this.settings.itemsPerPage;
-      this.renderArticles(Paginator.data);
+      this.filteredData = output;
+      this.actualPage = 1;
+      this.actualItem = 0;
+      this.actualLastItem = this.settings.itemsPerPage;
+      this.renderArticles(this.filteredData);
     }
   };
 
-  // resize fix - resize_toggle is set to true when 
-  this.resize_toggle = true;
-
+  // resize fix - fixes issue when there are more articles loaded through "load more" button and then screen is resized
   Paginator.prototype.resize = function()
   {
     var width = window.innerWidth;
     if (width < 460) {
-      resize_toggle = true;
+      this.resize_toggle = true;
     }
-    if (width > 460 && resize_toggle) {
-      resize_toggle = false;
+    if (width > 460 && this.resize_toggle) {
+      this.resize_toggle = false;
       document.getElementById(this.settings.container).innerHTML = "";
-      this.renderArticles(Paginator.data);
+      this.renderArticles(this.data);
     }
   }
 
-  var paginator = new Paginator();
+  Paginator.prototype.changePage = function(event) {
+    var page_target = event.target;
+    var attr = page_target.getAttribute("id");
+    if (attr === "next") {
+      this.nextPage();
+    }
+    else if (attr === "previous") {
+      this.previousPage();
+    }
+    else if (attr === "page") {
+      var page_number = page_target.getAttribute("data-page");
+      page_number = parseInt(page_number);
+      this.concretePage(page_number);
+    }
+  }
+
+
+  var myPagin = new Paginator({});
+  load(myPagin.settings.url, myPagin.init.bind(myPagin));
   // pagination listeners
-  document.getElementById("next").addEventListener("click", paginator.nextPage);
-  document.getElementById("previous").addEventListener("click", paginator.previousPage);
+  document.getElementById("pagin").addEventListener("click", myPagin.changePage.bind(myPagin));
   // "load more" button in mobile version
-  document.getElementById("load").addEventListener("click", paginator.loadMore);
-  document.getElementById("categories").addEventListener("click", paginator.filter);
+  document.getElementById("load").addEventListener("click", myPagin.loadMore.bind(myPagin));
+  document.getElementById("categories").addEventListener("click", myPagin.filter.bind(myPagin));
   // listener for pages (1,2,3, etc...)
-  document.getElementById("pages").addEventListener("click", paginator.concretePage);
-  var links = document.getElementById("pages").getElementsByTagName("a");   // targets all page links in "div"
+  // document.getElementById("pages").addEventListener("click", myPagin.concretePage.bind(myPagin));
+  // var links = document.getElementById("pages").getElementsByTagName("a");   // targets all page links in "div"
   
-  window.onresize = paginator.resize;
+  
+  
+  window.onresize = myPagin.resize.bind(myPagin);
 } (window, document))
 
